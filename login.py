32021871b73daa5624fa0c628a93607cb5676a78
Julio-PC\Julio <julio.llinas@gmail.com>
2025-01-16 22:03:45 -0400

    create img bg

--- login.py
+++ login.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 class Login(tk.Frame):
     
@@ -14,6 +16,12 @@ class Login(tk.Frame):
         fondo = tk.Frame(self, bg='#C6D9E3')
         fondo.pack()
         fondo.place(x = 0, y = 0, width = 1100, height = 650)
+        
+        self.bg_image = Image.open('media/img/fondo.png')
+        self.bg_image = self.bg_image.resize((1100, 650))
+        self.bg_image = ImageTk.PhotoImage(self.bg_image)
+        self.bg_label = ttk.Label(fondo, image = self.bg_image)
+        self.bg_label.place(x = 0, y = 0, width = 1100, height = 650 )
     
 class Registro(tk.Frame):
     
