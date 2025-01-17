689b9bfb44dacce617d9bfda8f6837eb3b93d8b0
Julio-PC\Julio <julio.llinas@gmail.com>
2025-01-16 21:47:03 -0400

    update requirements.txt

--- login.py
+++ login.py
@@ -5,11 +5,15 @@ class Login(tk.Frame):
     
     def __init__(self, padre, controlador):
         super().__init__(padre)
+        self.pack()
+        self.place(x = 0, y = 0, width = 1100, height = 650)
         self.controlador = controlador
         self.widgets()
         
     def widgets(self):
-        pass
+        fondo = tk.Frame(self, bg='#C6D9E3')
+        fondo.pack()
+        fondo.place(x = 0, y = 0, width = 1100, height = 650)
     
 class Registro(tk.Frame):
     
